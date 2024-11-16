ee9059db3e0d8a4355802f28a73b3671508f18a5
cas-png <72526687+cas-png@users.noreply.github.com>
2024-11-16 22:46:47 +0100

    makes illegal moves not possible

--- team07_A0/sudokuai.py
+++ team07_A0/sudokuai.py
@@ -1,7 +1,3 @@
-#  (C) Copyright Wieger Wesselink 2021. Distributed under the GPL-3.0-or-later
-#  Software License, (See accompanying file LICENSE or copy at
-#  https://www.gnu.org/licenses/gpl-3.0.txt)
-
 import random
 import time
 from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
@@ -16,21 +12,77 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     def __init__(self):
         super().__init__()
 
-    # N.B. This is a very naive implementation.
+    # Improved implementation that ensures only legal moves are made.
     def compute_best_move(self, game_state: GameState) -> None:
         N = game_state.board.N
 
-        # Check whether a cell is empty, a value in that cell is not taboo, and that cell is allowed
-        def possible(i, j, value):
-            return game_state.board.get((i, j)) == SudokuBoard.empty \
-                   and not TabooMove((i, j), value) in game_state.taboo_moves \
-                       and (i, j) in game_state.player_squares()
+        # Check if a move is valid according to Sudoku rules
+        def is_valid_move(i, j, value):
+            # Cell must be empty
+            if game_state.board.get((i, j)) != SudokuBoard.empty:
+                return False
 
-        all_moves = [Move((i, j), value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
+            # Move must not be taboo
+            if TabooMove((i, j), value) in game_state.taboo_moves:
+                return False
+
+            # Move must be allowed for the player
+            if (i, j) not in game_state.player_squares():
+                return False
+
+            # Sudoku rule: 'value' must not already exist in the same row, column, or grid
+            for k in range(N):
+                # Check row and column
+                if game_state.board.get((i, k)) == value or game_state.board.get((k, j)) == value:
+                    return False
+
+            # Check the 3x3 subgrid (assuming standard 9x9 Sudoku)
+            subgrid_size = int(N**0.5)
+            start_row = (i // subgrid_size) * subgrid_size
+            start_col = (j // subgrid_size) * subgrid_size
+
+            for x in range(start_row, start_row + subgrid_size):
+                for y in range(start_col, start_col + subgrid_size):
+                    if game_state.board.get((x, y)) == value:
+                        return False
+
+            return True
+
+        # Generate all valid moves
+        all_moves = [
+            Move((i, j), value)
+            for i in range(N)
+            for j in range(N)
+            for value in range(1, N + 1)
+            if is_valid_move(i, j, value)
+        ]
+
+        # If there are no valid moves, do nothing
+        if not all_moves:
+            print("No valid moves available.")
+            return
+
+        # Select a random valid move and propose it
         move = random.choice(all_moves)
         self.propose_move(move)
+
+        # Keep proposing valid moves continuously
         while True:
             time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
 
+            # Regenerate the list of valid moves to account for state changes
+            all_moves = [
+                Move((i, j), value)
+                for i in range(N)
+                for j in range(N)
+                for value in range(1, N + 1)
+                if is_valid_move(i, j, value)
+            ]
+
+            # Break if no moves are left (this might indicate game completion)
+            if not all_moves:
+                print("No valid moves remaining.")
+                break
+
+            # Propose another valid move
+            self.propose_move(random.choice(all_moves))
