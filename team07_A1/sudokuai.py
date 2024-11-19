302f8d1c2d91fa0207954b3aab3b0712a89f8cd1
Sviatoslav Gladkykh <s_gladkykh@wargaming.net>
2024-11-19 12:12:15 +0100

    added basic minimax

--- team07_A1/sudokuai.py
+++ team07_A1/sudokuai.py
@@ -1,11 +1,9 @@
-#  (C) Copyright Wieger Wesselink 2021. Distributed under the GPL-3.0-or-later
-#  Software License, (See accompanying file LICENSE or copy at
-#  https://www.gnu.org/licenses/gpl-3.0.txt)
-
+import copy
 import random
 import time
-from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
+
 import competitive_sudoku.sudokuai
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
 
 
 class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
@@ -16,63 +14,217 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     def __init__(self):
         super().__init__()
 
-    # N.B. This is a very naive implementation.
+    @staticmethod
+    def possible(game_state, i, j, value):
+        """check if move is possible in the current state of the game"""
+
+        # check if taboo move
+        if TabooMove((i, j), value) in game_state.taboo_moves:
+            return False
+
+        # check if cell is empty
+        if game_state.board.get((i, j)) != SudokuBoard.empty:
+            return False
+
+        # check horizontal line
+        for idx in range(game_state.board.N):
+            if game_state.board.get(square=(i, idx)) == value:
+                return False
+
+        # check vertical line
+        for idx in range(game_state.board.N):
+            if game_state.board.get(square=(idx, j)) == value:
+                return False
+
+        # check region
+        region_i = i // game_state.board.m
+        region_j = j // game_state.board.n
+
+        for i_in in range(game_state.board.m):
+            for j_in in range(game_state.board.n):
+                ind_i = (region_i * game_state.board.m) + i_in
+                ind_j = (region_j * game_state.board.n) + j_in
+
+                if game_state.board.get(square=(ind_i, ind_j)) == value:
+                    return False
+
+        return True
+
+    @staticmethod
+    def evaluate_game(game_state: GameState):
+        """
+        calculate score for the game (player_1_score - player_2_score)
+        player score is the number of row/columns/blocks closed
+        """
+
+        # check rows
+        closed_rows_1 = 0
+        closed_rows_2 = 0
+        for i in range(game_state.board.N):
+            cnt_1 = 0
+            cnt_2 = 0
+
+            for j in range(game_state.board.N):
+                if (i, j) in game_state.occupied_squares1:
+                    cnt_1 += 1
+                elif (i, j) in game_state.occupied_squares2:
+                    cnt_2 += 1
+
+            if cnt_1 == game_state.board.N:
+                closed_rows_1 += 1
+            elif cnt_2 == game_state.board.N:
+                closed_rows_2 += 1
+
+        # check cols
+        closed_cols_1 = 0
+        closed_cols_2 = 0
+        for i in range(game_state.board.N):
+            cnt_1 = 0
+            cnt_2 = 0
+            for j in range(game_state.board.N):
+                if (j, i) in game_state.occupied_squares1:
+                    cnt_1 += 1
+                elif (j, i) in game_state.occupied_squares2:
+                    cnt_2 += 1
+
+            if cnt_1 == game_state.board.N:
+                closed_cols_1 += 1
+            elif cnt_2 == game_state.board.N:
+                closed_cols_2 += 1
+
+        # check blocks
+        closed_blocks_1 = 0
+        closed_blocks_2 = 0
+        for b_i in range(game_state.board.N // game_state.board.m):
+            for b_j in range(game_state.board.N // game_state.board.n):
+
+                cnt_1 = 0
+                cnt_2 = 0
+
+                for i in range(game_state.board.m):
+                    for j in range(game_state.board.n):
+
+                        if (
+                            b_i * game_state.board.m + i,
+                            b_j * game_state.board.n + j,
+                        ) in game_state.occupied_squares1:
+                            cnt_1 += 1
+                        elif (
+                            b_i * game_state.board.m + i,
+                            b_j * game_state.board.n + j,
+                        ) in game_state.occupied_squares2:
+                            cnt_2 += 1
+
+                if cnt_1 == game_state.board.N:
+                    closed_blocks_1 += 1
+                if cnt_2 == game_state.board.N:
+                    closed_blocks_2 += 1
+
+        player_1_score = closed_rows_1 + closed_cols_1 + closed_blocks_1
+        player_2_score = closed_rows_2 + closed_cols_2 + closed_blocks_2
+
+        return player_1_score - player_2_score
+
+    @staticmethod
+    def minimax(game_state, depth):
+        """
+        minimax tree search algorithm up to a given depth
+        """
+        if depth == 0:
+            return SudokuAI.evaluate_game(game_state), None
+
+        # extract possible moves
+        player_squares = game_state.player_squares()
+
+        if player_squares is None:
+            player_squares = [(i, j) for i in range(game_state.board.N) for j in range(game_state.board.N)]
+
+        possible_moves = [
+            Move((i, j), value)
+            for i, j in player_squares
+            for value in range(1, game_state.board.N + 1)
+            if SudokuAI.possible(game_state, i, j, value)
+        ]
+
+
+        if len(possible_moves) == 0:
+            # terminate recursion if there are no possible moves
+            return SudokuAI.evaluate_game(game_state), None
+
+
+        if game_state.current_player == 1:
+            # maximizing case
+            value = float("-inf")
+            best_move = None
+
+            for move in possible_moves:
+                new_state = copy.deepcopy(game_state)
+
+                # change board, current player, occupied squares
+                new_state.board.put(move.square, move.value)
+                new_state.occupied_squares1.append(move.square)
+                new_state.current_player = 2
+
+                next_value, _ = SudokuAI.minimax(new_state, depth - 1)
+                if next_value > value:
+                    value = next_value
+                    best_move = move
+
+            return value, best_move
+
+        elif game_state.current_player == 2:
+            # minimizing case
+            value = float("inf")
+            best_move = None
+
+            for move in possible_moves:
+                new_state = copy.deepcopy(game_state)
+
+                # change board, current player, occupied squares
+                new_state.board.put(move.square, move.value)
+                new_state.occupied_squares2.append(move.square)
+                new_state.current_player = 1
+
+                next_value, _ = SudokuAI.minimax(new_state, depth - 1)
+                if next_value < value:
+                    value = next_value
+                    best_move = move
+
+            return value, best_move
+
+    @staticmethod
+    def select_move(game_state: GameState) -> Move:
+        """Select MINIMAX move if found and RANDOM otherwise"""
+        val, move = SudokuAI.minimax(game_state, depth=3)
+
+        if move is not None:
+            # minimax found move
+            print("*" * 100)
+            print(f"Selected MINIMAX move with score {val} | square = {move.square}; value = {move.value};")
+            print("*" * 100)
+            return move
+
+        # if minimax couldn't find a move, just take random
+        player_squares = game_state.player_squares()
+        if player_squares is None:
+            player_squares = [(i, j) for i in range(game_state.board.N) for j in range(game_state.board.N)]
+        possible_moves = [
+            Move((i, j), value)
+            for i, j in player_squares
+            for value in range(1, game_state.board.N + 1)
+            if SudokuAI.possible(game_state, i, j, value)
+        ]
+
+        move = random.choice(possible_moves)
+        print("*" * 100)
+        print(f"Selected RANDOM move | square = {move.square}; value = {move.value};")
+        print("*" * 100)
+        return move
+
     def compute_best_move(self, game_state: GameState) -> None:
-        N = game_state.board.N
 
-        # Check whether a cell is empty, a value in that cell is not taboo, and that cell is allowed
-        def possible(i, j, value):
-            return game_state.board.get((i, j)) == SudokuBoard.empty \
-                   and not TabooMove((i, j), value) in game_state.taboo_moves \
-                       and (i, j) in game_state.player_squares()
-        
-        # Calculate the score of a move
-        def score(i, j, value, game_state: GameState):
-            # Put the move on the board
-            game_state.board.put((i, j), value)
+        self.propose_move(self.select_move(game_state))
 
-            N = game_state.board.N
-            n = game_state.board.n      # Number of rows
-            m = game_state.board.m      # Number of columns
-            reg_completed = 0
-
-            # Check the move completes a column
-            full_col = True
-            for k in range(N):
-                if game_state.board.get((k, j)) == SudokuBoard.empty:
-                    full_col = False
-            if full_col:
-                reg_completed += 1
-
-            # Check the move completes a row
-            full_row = True
-            for l in range(N):
-                if game_state.board.get((i, l)) == SudokuBoard.empty:
-                    full_row = False
-            if full_row:
-                reg_completed += 1
-
-            # Check the move completes a rectangle
-            full_rectangle = True
-            # Get coordinates for the left upper corner in the n by m rectangle where (i,j) is located
-            i_left_top = i - (i % n)
-            j_left_top = j - (j % m)
-            for k in range(n):
-                for l in range(m):
-                    if game_state.board.get((i_left_top + k, j_left_top + l)) == SudokuBoard.empty:
-                        full_rectangle = False
-            if full_rectangle:
-                reg_completed += 1
-            
-            scores = [0, 1, 3, 7]
-
-            return scores[reg_completed]
-
-        all_moves = [Move((i, j), value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
-        move = random.choice(all_moves)
-        self.propose_move(move)
         while True:
             time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
-
+            self.propose_move(self.select_move(game_state))
