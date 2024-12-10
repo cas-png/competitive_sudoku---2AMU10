7b26f708c5cc2767aaa41c64e4f9cbf696e29203
Sviatoslav Gladkykh <s_gladkykh@wargaming.net>
2024-12-10 14:20:38 +0100

    upd depth

--- team07_A2/sudokuai.py
+++ team07_A2/sudokuai.py
@@ -327,7 +327,7 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         return value, best_move
 
     def compute_best_move(self, game_state: GameState) -> None:
-        for depth in range(2, game_state.board.N**2):
+        for depth in range(1, game_state.board.N**2):
             val, move = self.minimax(game_state, depth=depth)
             print("*" * 100)
             print(f"Selected MINIMAX move - {move} with score - {val} | depth - {depth}")
